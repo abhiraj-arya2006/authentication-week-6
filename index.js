d2e917da3b196cb33e05e94b823ee2d365a6f4ef
abhiraj-arya2006 <arya2006abhiraj12345@gmail.com>
2025-12-09 13:48:17 +0530

    "Init"

---
+++ index.js
@@ -0,0 +1,85 @@
+import express from 'express';
+import jwt, { decode } from 'jsonwebtoken';
+
+const JWT_SECRET="abhiraj@hero1"
+
+const app =express();
+
+app.use(express.json());
+const users=[];
+
+app.post('/signup',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    users.push({
+        username:username,
+        password:password
+    })
+    res.json({
+        message:"You are signed in."
+    })
+})
+
+app.post('/signin',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    let user=null;
+
+    user=users.find(function(u){
+        if(u.username==username && u.password ==password){
+            return true;
+        }else{
+            return false;
+        }
+    })
+    if(user){
+        const token= jwt.sign({
+            username:username
+        },JWT_SECRET);
+        res.json({
+            token:token
+        })
+        console.log(users);
+        
+    }else{
+        res.json({
+            message: "Credentials incorrect."
+        })
+        return
+    }
+    
+})
+//middleware
+function auth(req,res,next){
+    const token = req.headers.token;
+    const decodedData=jwt.verify(token,JWT_SECRET);
+    if(req.username==username){
+        next()
+    }else{
+        req.json({
+            message:"You are not logged in."
+        })
+    }
+    
+}
+
+app.get('/me',auth,function(req,res){
+    // const token = req.headers.token;
+    // const decodedData=jwt.verify(token,JWT_SECRET);
+
+    if(decodedData.username){
+        let foundUser=null;
+        for(let i=0;i<users.length;i++){
+            if(users[i].username===req.username){
+                foundUser=users[i];
+            }
+        }
+        res.json({
+            username:foundUser.username,
+            password:foundUser.password
+        })
+    }
+})
+
+app.listen(3000);
