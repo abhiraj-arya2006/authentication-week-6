fc802a5762e9271a3617a160d67a3157f9939659
abhiraj-arya2006 <arya2006abhiraj12345@gmail.com>
2026-01-06 15:14:53 +0530

    .

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import jwt, { decode } from 'jsonwebtoken';
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 const JWT_SECRET="abhiraj@hero1"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app =express();
 
 app.use(express.json());
 const users=[];
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/",function(req,res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
 
 app.post('/signup',function(req,res){
     const username=req.body.username;
